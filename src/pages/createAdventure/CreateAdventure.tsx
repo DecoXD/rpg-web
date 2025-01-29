ad3acdf3f4363c0c9328a4feeb9d5868b64b59ca
unknown <deco.xd@outlook.com>
2025-01-29 11:28:19 -0300

    feat:create CreateAdventure form UI

--- src/pages/createAdventure/CreateAdventure.tsx
+++ src/pages/createAdventure/CreateAdventure.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { Textarea } from "@/components/ui/textarea";
@@ -12,14 +13,14 @@ const chooseCorrectInput = ({label,name,type,placeholder,inputStyle,options}:Cre
   switch (inputStyle) {
     case 'textarea':
       return (
-        <Textarea className="w-full" name={name} placeholder={placeholder}/>
+        <Textarea className="w-full " name={name} placeholder={placeholder}/>
       )
       break;
     case 'select':
       return (
         <Select>
           <SelectTrigger className="w-[180px]">
-            < SelectValue placeholder="Select a fruit" />
+            < SelectValue placeholder={placeholder} />
           </SelectTrigger>
           <SelectContent>
             <SelectGroup>
@@ -32,7 +33,7 @@ const chooseCorrectInput = ({label,name,type,placeholder,inputStyle,options}:Cre
       break
   
     default:
-      <Input type={type} name={name}/>
+      return<Input type={type} name={name} placeholder={placeholder}/>
       break;
   }
 }
@@ -43,18 +44,24 @@ const CreateAdventure = () => {
     <div className='pt-20 w-full'>
       <h2>Criar Aventura</h2>
 
-      <form action="">
+      <form className="w-full max-w-2xl flex flex-col gap-4 p-8 bg-white shadow-lg rounded-lg">
         {
           createAdventureInputs.map((item)  =>{
             return(
-              <label htmlFor="">
-                <span>{item.label}</span>
-                {chooseCorrectInput(item)}
+            <div className="w-full">
+                <label className="flex flex-col gap-2 capitalize font-medieval text-lg font-medium"  >
+                  <span>{item.label}</span>
+                  {
+                    chooseCorrectInput(item)
+                  }
               </label>
+            </div>
             )
           })
         }
-
+      <Button className="bg-amber-300 mt-4 text-black font-medieval font-bold hover:bg-amber-300/90 uppercase ">
+        Create
+      </Button>
       </form>
     </div>
   )
