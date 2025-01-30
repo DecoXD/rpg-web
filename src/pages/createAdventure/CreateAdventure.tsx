cd20c1680088913c1cef89fbde5fee4aed01b12a
decoXD <deco.xd@outlook.com>
2025-01-29 21:18:36 -0300

    style(CreateAdventure):implements display flex and box shadow

--- src/pages/createAdventure/CreateAdventure.tsx
+++ src/pages/createAdventure/CreateAdventure.tsx
@@ -4,6 +4,7 @@ import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrig
 import { Textarea } from "@/components/ui/textarea";
 import { createAdventureInputs } from "@/constantes";
 import { CreateAdventureInputsProps } from "@/types/ui"
+import { BookText } from "lucide-react";
 
 
 
@@ -41,10 +42,13 @@ const chooseCorrectInput = ({label,name,type,placeholder,inputStyle,options}:Cre
 
 const CreateAdventure = () => {
   return (
-    <div className='pt-20 w-full'>
-      <h2>Criar Aventura</h2>
+    <div className='pt-20 bg-slate-50 p-2 w-full flex flex-col gap-10 justify-center items-center'>
+      <h2 className="text-2xl font-bold font-medieval flex items-center gap-2">
+        <BookText className="text-amber-500" size={30}/>
+        <span>Criar Aventura</span>
+      </h2>
 
-      <form className="w-full max-w-2xl flex flex-col gap-4 p-8 bg-white shadow-lg rounded-lg">
+      <form className="w-full max-w-2xl flex flex-col gap-4 p-8 bg-white shadow-2xl rounded-lg">
         {
           createAdventureInputs.map((item)  =>{
             return(
